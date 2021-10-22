24886d007c79d9c11034a9c1df362a6f9e786ac9
gdlcf88 <gdlcf88@outlook.com>
2021-10-22 17:31:25 +0800

    Ignore ExtraProperties when mapping from view models.

--- src/EasyAbp.Abp.DynamicMenu.Web/DynamicMenuWebAutoMapperProfile.cs
+++ src/EasyAbp.Abp.DynamicMenu.Web/DynamicMenuWebAutoMapperProfile.cs
@@ -1,6 +1,7 @@
 using EasyAbp.Abp.DynamicMenu.MenuItems.Dtos;
 using AutoMapper;
 using EasyAbp.Abp.DynamicMenu.Web.Pages.Abp.DynamicMenu.MenuItems.MenuItem.ViewModels;
+using Volo.Abp.AutoMapper;
 
 namespace EasyAbp.Abp.DynamicMenu.Web
 {
@@ -12,8 +13,8 @@ namespace EasyAbp.Abp.DynamicMenu.Web
              * Alternatively, you can split your mapping configurations
              * into multiple profile classes for a better organization. */
             CreateMap<MenuItemDto, EditMenuItemViewModel>();
-            CreateMap<CreateMenuItemViewModel, CreateMenuItemDto>();
-            CreateMap<EditMenuItemViewModel, UpdateMenuItemDto>();
+            CreateMap<CreateMenuItemViewModel, CreateMenuItemDto>().Ignore(x => x.ExtraProperties);
+            CreateMap<EditMenuItemViewModel, UpdateMenuItemDto>().Ignore(x => x.ExtraProperties);
         }
     }
 }
