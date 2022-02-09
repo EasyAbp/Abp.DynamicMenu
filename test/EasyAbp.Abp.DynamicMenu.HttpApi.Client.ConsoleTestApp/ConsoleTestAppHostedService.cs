f2c00fe967e6eac6bba9bea0b4056bf03788f27a
gdlcf88 <gdlcf88@outlook.com>
2022-02-10 00:58:26 +0800

    Upgrade to ABP 5.1.3

--- test/EasyAbp.Abp.DynamicMenu.HttpApi.Client.ConsoleTestApp/ConsoleTestAppHostedService.cs
+++ test/EasyAbp.Abp.DynamicMenu.HttpApi.Client.ConsoleTestApp/ConsoleTestAppHostedService.cs
@@ -18,17 +18,17 @@ namespace EasyAbp.Abp.DynamicMenu.HttpApi.Client.ConsoleTestApp
 
         public async Task StartAsync(CancellationToken cancellationToken)
         {
-            using (var application = AbpApplicationFactory.Create<DynamicMenuConsoleApiClientModule>(options=>
+            using (var application = await AbpApplicationFactory.CreateAsync<DynamicMenuConsoleApiClientModule>(options=>
             {
                 options.Services.ReplaceConfiguration(_configuration);
             }))
             {
-                application.Initialize();
+                await application.InitializeAsync();
 
                 var demo = application.ServiceProvider.GetRequiredService<ClientDemoService>();
                 await demo.RunAsync();
 
-                application.Shutdown();
+                await application.ShutdownAsync();
             }
         }
 
